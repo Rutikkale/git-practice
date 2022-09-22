5e566d37d35a60a8be49ed21a83ef5cc7635bcfb
Rutikkale <rutikkale17@gmail.com>
2022-09-22 14:56:48 +0530

    frist program

---
+++ 29Mar/p2.C
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+int num=200;
+clrscr();
+printf("%d",num);
+getch();
+}
---
+++ 29Mar/p3.C
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main()
+{
+int num=520;
+clrscr();
+printf("the value is= %d",num);
+getch();
+}
