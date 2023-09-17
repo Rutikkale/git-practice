d33f0c9de6b32f15040b2b1ae7df8413b214c803
Rutikkale <rutikkale17@gmail.com>
2023-09-17 20:46:44 +0530

    relation_operatores

---
+++ ralational_operators/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ralational_operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = 10;
+            //int b = 10;
+
+            //bool c = a == b;
+
+            //int a = 10;
+            //int b = 10;
+
+            //bool c = a != b;
+
+
+            //int a = 10;
+            //int b = 10;
+
+            //bool c = a > b;
+
+
+            //int a = 10;
+            //int b = 10;
+
+            //bool c = a < b;
+
+            //int a = 20;
+            //int b = 10;
+
+            //bool c = a >= b;
+
+            int a = 20;
+            int b = 10;
+
+            bool c = a <= b;
+
+
+
+
+
+            Console.WriteLine(c);
+            Console.ReadLine();
+        }
+    }
+}
